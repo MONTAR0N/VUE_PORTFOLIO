1822282de3fce9b1789d3d7883595e3f6182f72d
Vicente Avilés <138259837+MONTAR0N@users.noreply.github.com>
2024-02-22 17:28:19 -0300

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Landing from './components/Landing.vue'
+</script>
+
+<template>
+  <header>
+    <div >
+      <Landing />
+    </div>
+  </header>
+
+  <main>
+   
+  </main>
+
+  <body>
+    
+  </body>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-container">
+    <div class="profile">
+      <img src="" alt="" />
+      <h3>Vicente Avilés</h3>
+      <p>Software Developer</p>
+    </div>
+    <div class="social-links">
+      <a href="https://github.com/MONTAR0N">
+        <div class="link-div">
+          <img src="" alt="" />
+          <h2>Github</h2>
+        </div>
+      </a>
+      <a href="https://www.linkedin.com/in/vicente-avil%C3%A9s-93a63b171/">
+        <div class="link-div">
+          <img src="" alt="" />
+          <h2>LinkedIn</h2>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
