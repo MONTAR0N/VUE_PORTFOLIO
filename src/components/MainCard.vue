639ad5bae715d274ab645616d684904ee42548f9
Vicente Avilés <138259837+MONTAR0N@users.noreply.github.com>
2024-02-23 19:56:19 -0300

    mainCardStyles

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -9,14 +9,18 @@
       <div class="social-links">
         <div class="link-div">
           <a href="https://github.com/MONTAR0N">
-            <img src="../assets/github-logo-icon.png" alt="" class="logo-img" />
+            <img
+              src="../components/icons/icons8-github-50.png"
+              alt=""
+              class="logo-img"
+            />
             <h3>Github</h3>
           </a>
         </div>
         <div class="link-div">
           <a href="https://www.linkedin.com/in/vicente-avil%C3%A9s-93a63b171/">
             <img
-              src="../assets/icons8-linkedin-48.png"
+              src="../components/icons/icons8-linkedin-50.png"
               alt=""
               class="logo-img"
             />
@@ -25,14 +29,18 @@
         </div>
         <div class="link-div">
           <a href="mailto:vavilesb1@gmail.com">
-            <img src="../assets/gmail-logo-4-1.png" alt="" class="logo-img" />
+            <img
+              src="../components/icons/icons8-mail-50 (1).png"
+              alt=""
+              class="logo-img"
+            />
             <h3>Gmail</h3>
           </a>
         </div>
         <div class="link-div">
           <a href="https://wa.me/56977556547">
             <img
-              src="../assets/whatsapp-logo-4-1.png"
+              src="../components/icons/icons8-whatsapp-50.png"
               alt=""
               class="logo-img"
             />
@@ -52,96 +60,102 @@
           obcaecati. Voluptatem accusamus totam corporis.
         </p>
       </div>
-      <div class="technology-section">
+      <div class="techContainer">
         <h1>Languages and Tools</h1>
-        <p>
-          <a href="https://www.w3schools.com/css/" target="_blank">
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
-              alt="css3"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a href="https://expressjs.com" target="_blank">
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
-              alt="express"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a href="https://es.react.dev/" target="_blank">
-            <img
-              src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"
-              alt="react"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a href="https://es.redux.js.org/" target="_blank">
-            <img
-              src="https://www.svgrepo.com/show/303557/redux-logo.svg"
-              alt="redux"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a href="https://www.w3.org/html/" target="_blank">
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
-              alt="html5"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-            target="_blank"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-              alt="javascript"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a href="https://nodejs.org" target="_blank">
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
-              alt="nodejs"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a href="https://vuejs.org//" target="_blank">
-            <img
-              src="https://www.vectorlogo.zone/logos/vuejs/vuejs-icon.svg"
-              alt="Vue.js"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a href="https://sequelize.org/" target="_blank">
-            <img
-              src="https://www.vectorlogo.zone/logos/sequelizejs/sequelizejs-icon.svg"
-              alt="sequelize"
-              width="40"
-              height="40"
-            />
-          </a>
-          <a href="https://www.postgresql.org/" target="_blank">
-            <img
-              src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg"
-              alt="postgreSQL"
-              width="40"
-              height="40"
-            />
-          </a>
-        </p>
+        <div class="technology-section">
+          <p>
+            <a href="https://www.w3schools.com/css/" target="_blank">
+              <img
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                alt="css3"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a href="https://expressjs.com" target="_blank">
+              <img
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+                alt="express"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a href="https://es.react.dev/" target="_blank">
+              <img
+                src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"
+                alt="react"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a href="https://es.redux.js.org/" target="_blank">
+              <img
+                src="https://www.svgrepo.com/show/303557/redux-logo.svg"
+                alt="redux"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a href="https://www.w3.org/html/" target="_blank">
+              <img
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                alt="html5"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+              target="_blank"
+            >
+              <img
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                alt="javascript"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a href="https://nodejs.org" target="_blank">
+              <img
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+                alt="nodejs"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a href="https://vuejs.org//" target="_blank">
+              <img
+                src="https://www.vectorlogo.zone/logos/vuejs/vuejs-icon.svg"
+                alt="Vue.js"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a href="https://sequelize.org/" target="_blank">
+              <img
+                src="https://www.vectorlogo.zone/logos/sequelizejs/sequelizejs-icon.svg"
+                alt="sequelize"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a href="https://www.postgresql.org/" target="_blank">
+              <img
+                src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg"
+                alt="postgreSQL"
+                width="40"
+                height="40"
+              />
+            </a>
+          </p>
+        </div>
       </div>
 
       <div class="buttonContainer">
+        <h1>Also see</h1>
+        <div class="buttonSub">
+
+        
         <div class="button">
           <a href="">
             <h2>My Proyects</h2>
@@ -151,6 +165,7 @@
           <a href="">
             <h2>My Resumé</h2>
           </a>
+        </div>
         </div>
       </div>
     </div>
@@ -169,26 +184,29 @@
   background-color: black;
   margin: 5%;
   border-radius: 20px;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);
+
   /* flex-direction: column; */
   /* align-items: center; */
 }
 .container1 {
-  background-color: rgb(245, 243, 128);
+  background-color: #e8ebf8;
   width: 25%;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 3rem;
   flex-direction: column;
-  margin: 5px;
+  margin: 5px 0 5px 5px;
   border-radius: 20px;
 }
 .container1 a {
   text-decoration: none;
+  color: inherit;
 }
 .container2 {
   display: flex;
-  background-color: rgb(235, 250, 191);
+  background-color: #b2bff7;
   color: rgb(0, 0, 0);
   width: 75%;
   flex-direction: column;
@@ -201,10 +219,12 @@
 
 .container2 a {
   text-decoration: none;
+  color: inherit;
 }
 .profile img {
   width: 6rem;
   border-radius: 10px;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);
 }
 .about {
   display: flex;
@@ -213,26 +233,49 @@
   text-align: left;
 }
 
+.about p{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #d3d9f5;
+  border-radius: 20px;
+  padding: 10px;
+}
+
 .buttonContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.buttonSub{
   display: flex;
   flex-direction: row;
 }
 .button {
-  border: solid black 1px;
+  border-radius: 20px;
   margin: 10px;
   padding-left: 5px;
   padding-right: 5px;
-  border-radius: 20px;
-  background-color: rgb(226, 243, 42);
+  background-color: #d3d9f5;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);
 }
 
 /* .about p{
   display: flex;
 } */
 
+.techContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .technology-section {
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-color: #d3d9f5;
+  border-radius: 20px;
+  padding: 5px;
 }
 </style>
